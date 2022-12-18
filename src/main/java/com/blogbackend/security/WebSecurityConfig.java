89aad7ef1d8660ef05a7a42d7e14df083aa87332
Adrian <cancadua@protonmail.com>
2022-12-18 12:41:10 +0100

    First test successful

---
+++ src/main/java/com/blogbackend/security/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.blogbackend.security;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/public/**").permitAll()
+                .anyRequest();
+        return http.build();
+    }
+}
--- src/test/java/com/blogbackend/controllers/PostControllerTest.java
+++ src/test/java/com/blogbackend/controllers/PostControllerTest.java
@@ -3,6 +3,7 @@ package com.blogbackend.controllers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -10,7 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = PostController.class)
+@WebMvcTest(controllers = PostController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class})
 @ExtendWith(SpringExtension.class)
 public class PostControllerTest {
 
